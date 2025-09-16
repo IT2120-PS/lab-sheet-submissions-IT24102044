dc5a918077415cfebba79303d71fe0434f212ad3
IT24102044 <it24102044@my.sliit.lk>
2025-09-16 18:23:49 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102044.R
@@ -0,0 +1,9 @@
+setwd('C:\\Users\\Dulmi Wijesighe\\Desktop\\IT24102044')
+#Q1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+#Q2
+pexp(2, rate = 1/3) 
+#Q3-part1
+1 - pnorm(130, mean = 100, sd = 15)
+#Q3-part2
+qnorm(0.95, mean = 100, sd = 15)
